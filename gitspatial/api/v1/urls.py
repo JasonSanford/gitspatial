17a409fd082cd5b8eea00d4fa593e846542db376
Jason Sanford <jason.sanford@mapmyfitness.com>
2013-08-06 08:52:28 -0600

    repo names can have periods

--- gitspatial/api/v1/urls.py
+++ gitspatial/api/v1/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('gitspatial.api.v1.views',
-    url(r'^(?P<user_name>[a-zA-Z0-9_-]*)/(?P<repo_name>[a-zA-Z0-9_-]*)/(?P<feature_set_name>.*)', 'feature_set_query', name='v1_feature_set_query'),
+    url(r'^(?P<user_name>[.a-zA-Z0-9_-]*)/(?P<repo_name>[.a-zA-Z0-9_-]*)/(?P<feature_set_name>.*)', 'feature_set_query', name='v1_feature_set_query'),
 )
