27925005abce2c1134f8cf31a4b7e955e7f329c8
Jason Sanford <jason.sanford@mapmyfitness.com>
2013-08-06 23:45:04 -0600

    Stubbing out some API stuff

--- gitspatial/api/v1/views.py
+++ gitspatial/api/v1/views.py
@@ -1,5 +1,38 @@
+from django.http import Http404
 from django.shortcuts import HttpResponse
+from django.contrib.gis.geos.polygon import Polygon
+
+from gitspatial.models import Repo, FeatureSet, Feature
 
 
-def feature_set_query(request, user_name=None, repo_name=None, feature_set_name=None):
-    return HttpResponse('User is {user}. Repo is {repo}. Feature Set is {feature_set}.'.format(user=user_name, repo=repo_name, feature_set=feature_set_name))
+def feature_set_query(request, user_name, repo_name, feature_set_name):
+    full_name = '{0}/{1}'.format(user_name, repo_name)
+    try:
+        repo = Repo.objects.get(full_name=full_name)
+        feature_set = FeatureSet.objects.get(repo=repo, name=feature_set_name)
+    except (Repo.DoesNotExist, FeatureSet.DoesNotExist):
+        raise Http404
+
+    filter_kwargs = {
+        'feature_set': feature_set,
+    }
+    spatial_args = None
+
+    if 'bbox' in request.GET:
+        spatial_args = _by_bbox(request.GET['bbox'])
+
+    if spatial_args is not None:
+        filter_kwargs.update(spatial_args)
+
+    features = Feature.objects.filter(**filter_kwargs)
+
+    return HttpResponse('Feature count is is {0}.'.format(len(features)))
+
+
+def _by_bbox(bbox_string):
+    bbox = bbox_string.split(',')
+    bbox = map(float, bbox)
+
+    return {
+        'geom__intersects': Polygon.from_bbox(bbox)
+    }
